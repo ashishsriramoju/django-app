43226c97d5ba7677f9cd73b7f58c998827855f23
ashishsriramoju <ashishsriramoju25@gmail.com>
2025-08-13 19:13:58 +0530

    changed the return type

--- myapp/views.py
+++ myapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello world, this is my first Django app!")
+    return HttpResponse("Hi Ashish, this is a simple Django app with a hello world view!")
 # Create your views here.
